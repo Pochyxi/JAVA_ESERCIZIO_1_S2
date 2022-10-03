782760a3c5f088e0c8198f2fa9675629401a6ae4
Pochyxi <adienerlopez@gmail.com>
2022-10-04 00:42:22 +0200

    adiener

--- src/POJO/Esercizio1.java
+++ src/POJO/Esercizio1.java
@@ -8,74 +8,98 @@ import java.util.Scanner;
 
 public class Esercizio1 {
 
-    private static final Logger logger = LoggerFactory.getLogger ( Esercizio1.class );
-    private int[] myArray = new int[10];
+    private static final Logger logger = LoggerFactory.getLogger( Esercizio1.class );
+    private int[] myArray = new int[ 10 ];
 
-    public Esercizio1() {
-        setMyArray ();
+    public Esercizio1()
+    {
+        setMyArray();
     }
 
-    public void setMyArray () {
-        for (int i = 0; i < myArray.length; i++) {
-            myArray[i] = (int)Math.floor ( Math.random () * 10 + 1 );
+    public void setMyArray()
+    {
+        for( int i = 0 ; i < myArray.length ; i++ ) {
+            myArray[ i ] = ( int ) Math.floor( Math.random() * 10 + 1 );
         }
     }
 
     @Override
-    public String toString () {
-        return Arrays.toString ( myArray );
+    public String toString()
+    {
+        return Arrays.toString( myArray );
     }
 
-    public int gameArray () {
-        Scanner scanner = new Scanner ( System.in );
+    public void gameArray()
+    {
+        Scanner scanner = new Scanner( System.in );
 
-        int[] array = new int[10];
+        int[] array = new int[ 10 ];
 
-        System.out.println (" Inserisci la posizione da modificare" );
+        boolean loop = true;
+
+        do {
+            System.out.println( Arrays.toString( myArray ) );
+            System.out.println( " Inserisci la posizione da modificare, posizioni accettate: 0 - 9" );
 
 
-        int posizione = 0;
-        boolean posizioneCheck = true;
+            int posizione = 0;
+            boolean posizioneCheck = true;
 
-        while (posizioneCheck) {
-            try {
-                 posizione = scanner.nextInt();
-                if ( posizione < 0  || posizione > 10 ) {
-                    logger.error ( "Posizione inserita non valida" );
-                } else {
-                    posizioneCheck = false;
+            while( posizioneCheck ) {
+                try {
+                    posizione = scanner.nextInt();
+                    if( posizione < 0 || posizione > 10 ) {
+                        logger.error( "Posizione inserita non valida, riprova!" );
+                    } else {
+                        posizioneCheck = false;
+                    }
+                } catch( Exception e ) {
+                    System.out.println( "Errore durante l'inserimento dell'indice, riprova!" );
+                    scanner.nextLine();
                 }
-            } catch (Exception e) {
-                System.out.println ( e.getMessage () );
-                scanner.nextLine ();
-            }
-        }
-
-        System.out.println ("Quale numero vuoi inserire alla posizione " + posizione + "?" );
-
-        int elementoPrima = myArray[posizione];
-        int elemento = scanner.nextInt ();
-
-        try {
-            if ( elemento < 0 || elemento > 10) {
-                logger.error("Posizione inserita fuori dal range, array non modificato");
-                elemento = elementoPrima;
             }
 
-        } catch (Exception e) {
-            logger.error("Errore durante l'inserimento dell'elemento, array non modificato");
-        }
+            System.out.println( "Quale numero vuoi inserire alla posizione " + posizione + "? Solo numeri da 1 a 10, 0 " +
+                    "per uscire." );
 
+            int elementoPrima = myArray[ posizione ];
+            int elemento = 0;
+            boolean elementoCheck = true;
 
-        for ( int i = 0; i < myArray.length; i++ ) {
-            if ( i == posizione) {
-                array[i] = elemento;
+            while( elementoCheck ) {
+                try {
+                    elemento = scanner.nextInt();
+                    if( elemento < 0 || elemento > 10 ) {
+                        logger.error( "Posizione inserita fuori dal range, array non modificato, riprova!" );
+                        elemento = elementoPrima;
+                    } else {
+                        elementoCheck = false;
+                    }
+
+                } catch( Exception e ) {
+                    logger.error( "Errore durante l'inserimento dell'elemento, array non modificato, riprova!" );
+                    scanner.nextLine();
+                }
+            }
+
+            if (elemento == 0) {
+                logger.info("Arrivederci!");
+                loop = false;
             } else {
-                array[i] = myArray[i];
+                for( int i = 0 ; i < myArray.length ; i++ ) {
+                    if( i == posizione ) {
+                        array[ i ] = elemento;
+                    } else {
+                        array[ i ] = myArray[ i ];
+                    }
+                }
+                myArray = array;
+                System.out.println( "FATTO!" );
             }
-        }
-        myArray = array;
-        System.out.println ("Ecco il tuo array!" + toString () );
-        return elemento;
+            
+
+            
+        } while( loop );
+
     }
 }
--- src/it/Adiener/core/Main.java
+++ src/it/Adiener/core/Main.java
@@ -2,23 +2,13 @@ package it.Adiener.core;
 
 
 import POJO.Esercizio1;
-import POJO.Esercizio2;
 
 public class Main {
     public static void main(String[] args) {
 
 //        ESERCIZIO 1
         Esercizio1 e1 = new Esercizio1();
-
-        int check = -1;
-
-        while (check != 0) {
-            e1.gameArray ();
-            System.out.println (e1.gameArray () );
-            check = e1.gameArray();
-        }
-
-
+        e1.gameArray ();
     }
 
 }
